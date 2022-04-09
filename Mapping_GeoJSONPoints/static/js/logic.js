2a097858dced3bd3cba308e746385505df418ed3
unknown <emjchou@gmail.com>
2022-04-09 16:13:28 -0700

    end of 13.5.4

--- Mapping_GeoJSONPoints/static/js/logic.js
+++ Mapping_GeoJSONPoints/static/js/logic.js
@@ -1,8 +1,7 @@
 // add console.log to check if code is working
 console.log("working");
 
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,8 +10,28 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Then we add tile layer to the map.
-streets.addTo(map);
+// create dart view tile layer for map option
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// create a base layer that holds both maps
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+}
+
+// Create the map object with center, zoom level, and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2, 
+    layers: [streets]
+});
+
+// pass map layers to layer control and add layers control to map
+L.control.layers(baseMaps).addTo(map);
 
 // accessing airport jeoJSON URL
 let airportData= "https://raw.githubusercontent.com/emjchou/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
@@ -34,4 +53,5 @@ d3.json(airportData).then(function(data){
     //create a FeoJSON layer with the retrieved data
     //L.geoJSON(data).addTo(map);
 })
+
 
